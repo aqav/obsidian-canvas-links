e9052b11483ad12d5def117f48f28edc80f2accf
aqav <66734646+aqav@users.noreply.github.com>
2023-04-20 18:10:30 +0800

    feat(main.ts): render canvas as outgoing links

--- main.ts
+++ main.ts
@@ -1,137 +1,181 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, ItemView, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile, WorkspaceLeaf } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+const VIEW_TYPE: string = "canvas-view"
 
-interface MyPluginSettings {
-	mySetting: string;
+export default class CanvasViewPlugin extends Plugin {
+
+    onload(): void {
+        this.registerView(VIEW_TYPE, (leaf) => new CanvasView(leaf))
+
+        this.addRibbonIcon("dice", "Load view", () => {
+            this.onloadView();
+        });
+    }
+
+    onunload(): void {
+        this.app.workspace.detachLeavesOfType(VIEW_TYPE);
+    }
+
+    async onloadView(): Promise<void> {
+        this.app.workspace.detachLeavesOfType(VIEW_TYPE);
+
+        await this.app.workspace.getRightLeaf(false).setViewState({
+            type: VIEW_TYPE,
+            active: true,
+        });
+
+        this.app.workspace.revealLeaf(
+            this.app.workspace.getLeavesOfType(VIEW_TYPE)[0]
+        );
+    }
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+class CanvasView extends ItemView {
+
+    constructor(leaf: WorkspaceLeaf) {
+        super(leaf);
+    }
+
+    getViewType(): string {
+        return VIEW_TYPE;
+    }
+
+    getDisplayText(): string {
+        return "Canvas View";
+    }
+
+    async onOpen(): Promise<void> {
+        const container = this.containerEl;
+        container.empty();
+
+        this.registerEvent(this.app.workspace.on('file-open', () => {
+            this.getCanvas().then((canvas) => {
+                this.renderCanvas(canvas, container);
+            });
+        }))
+    }
+
+    async getCanvas(): Promise<TFile[]> {
+        const activeFile: TFile | null = this.app.workspace.getActiveFile();
+        if (activeFile == null) {
+            return [];
+        }
+
+        const canvas: TFile[] = [];
+        const files: TFile[] = this.app.vault.getFiles();
+        for (const file of files) {
+            if ('canvas' == file.extension) {
+                canvas.push(file);
+            }
+        }
+
+        const canvasContent: Map<TFile, string> = new Map<TFile, string>();
+        for (const file of canvas) {
+            await this.app.vault.read(file).then((content: string) => {
+                canvasContent.set(file, content);
+            });
+        }
+
+        const canvasEmebeded: TFile[] = [];
+        for (const [file, content] of canvasContent) {
+            const nodes: node[] = JSON.parse(content).nodes;
+            for (const node of nodes) {
+                if ('file' == node.type && activeFile.name == node.file) {
+                    canvasEmebeded.push(file);
+                }
+            }
+        }
+
+        return canvasEmebeded;
+    }
+
+    renderCanvas(canvas: TFile[], container: Element): void {
+        const header: HTMLDivElement = container.createDiv({
+            cls: 'outgoing-link-pane node-insert-event',
+            attr: { 'style': 'position: relative;' },
+        });
+
+        header.createDiv({
+            cls: 'tree-item-self is-clickable',
+            attr: {
+                'aria-label': 'Click to collapse',
+                'aria-label-position': 'right'
+            }
+        }, (el) => {
+            el.createSpan({ cls: 'tree-item-icon collapse-icon' }, (el) => {
+                el.createSvg('svg', {
+                    attr: {
+                        'xmlns': 'http://www.w3.org/2000/svg',
+                        'width': '10',
+                        'height': '10',
+                        'viewBox': '0 0 24 24',
+                        'fill': 'none',
+                        'stroke': 'currentColor',
+                        'stroke-width': '4',
+                        'stroke-linecap': 'round',
+                        'stroke-linejoin': 'round',
+                        'cls': 'svg-icon right-triangle',
+                    }
+                });
+            });
+            el.createDiv({
+                cls: 'tree-item-inner',
+                text: 'Canvas'
+            });
+            el.createDiv({ cls: 'tree-item-flair-outer' }, (el) => {
+                el.createSpan({
+                    cls: 'tree-item-flair',
+                    text: canvas.length.toString()
+                })
+            });
+        });
+
+        const content:HTMLDivElement = header.createDiv({ cls: 'search-result-container' });
+        content.createDiv({
+            attr: {
+                'style': 'width: 1px; height: 0.1px; margin-bottom: 0px;'
+            }
+        });
+        for (const file of canvas) {
+            content.createDiv({
+                cls: 'tree-item-self is-clickable outgoing-link-item',
+                attr: { 'draggable': true }
+            }, (el) => {
+                el.createSpan({ cls: 'tree-item-icon' }, (el) => {
+                    el.createSvg('svg', {
+                        attr: {
+                            'xmlns': 'http://www.w3.org/2000/svg',
+                            'width': '16',
+                            'height': '16',
+                            'viewBox': '0 0 24 24',
+                            'fill': 'none',
+                            'stroke': 'currentColor',
+                            'stroke-width': '2',
+                            'stroke-linecap': 'round',
+                            'stroke-linejoin': 'round',
+                            'cls': 'svg-icon lucide-link',
+                        }
+                    }, (el) => {
+                        el.createSvg('path', {
+                            attr: { 'd': 'M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71' }
+                        });
+                        el.createSvg('path', {
+                            attr: { 'd': 'M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71' }
+                        });
+                    });
+                });
+                el.createDiv({
+                    cls: 'tree-item-inner',
+                    text: file.name
+                }).addEventListener('click', () => {
+                    this.app.workspace.openLinkText('', file.path);
+                });
+            });
+        }
+    }
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
-	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+type node = {
+    type: string
+    file: string
 }
